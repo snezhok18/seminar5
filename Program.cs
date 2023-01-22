e3aa2d8f274f6b17a2ace520aa5655916471ea91
Sergey <mma118@yandex.ru>
2023-01-22 15:09:14 +0800

    Added a solution task3

--- Program.cs
+++ Program.cs
@@ -34,24 +34,7 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-int[] rnd = arr(8);
-                                                        // Console.WriteLine($"{String.Join(" ~ ", rnd)}");
 
-int sum = 0;
-for (int i = 1; i < rnd.Length; i += 2)
-{
-        sum += rnd[i];
-}
-Console.WriteLine($"[{String.Join(", ", rnd)}] -> {sum} сумма нечетных элементов массива.");
-
-int[] arr(int size)
-{   int[] rnd = new int[size];
-    for (int i = 0; i < rnd.Length; i++)
-    {
-        rnd[i] = new Random().Next(15);
-    }
-    return rnd;
-}
 
 // Задача 37:
 // Найдите произведение пар чисел в одномерном массиве.
@@ -59,3 +42,39 @@ int[] arr(int size)
 // Результат запишите в новом массиве.
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
+
+
+    int[] rnd = GetArr(8);
+    int size = rnd.Length;
+    int[] newArr = new int[size];
+    int[] ArrSumm = new int[size/2];
+    int count = newArr.Length;
+    if (size%2 != 0)
+    {
+        Console.WriteLine("Число нечетное. Введите четное число");
+    }
+    else
+    {
+        for (int i = 0; i < rnd.Length; i++)        // Переворот массива
+        {
+            count -= 1;
+            newArr[count] = rnd[i];
+        }
+
+        for (int j = 0; j < ArrSumm.Length; j++)    //Суммированмие элементов массивов
+        {
+            ArrSumm[j] = rnd[j] + newArr[j];
+        }
+
+    Console.WriteLine($"[{String.Join(", ", rnd)}] -> [{String.Join(", ", ArrSumm)}]");
+    }
+
+    int[] GetArr(int size)                          // Метод заполнения массива случайными числами
+    {
+    int[] rnd = new int[size];
+    for (int i = 0; i < rnd.Length; i++)
+    {
+    rnd[i] = new Random().Next(15);  
+    }
+    return rnd;
+    }
